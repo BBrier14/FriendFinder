c31c96c1d3d881c465e8d23c2d4b6e7680d0a8ba
Benjamin Brier <ben.brier14@gmail.com>
2019-11-06 13:51:32 -0500

    created get request, needs post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,15 @@
 //================================
 const characters = require("../data/friends");
 
+// Routing
+//================================
+module.exports = function(app){
+
+    // Get request to display all possible matches
+    app.get("/api/characters", function (req, res){
+        res.json(characters);
+    });
+
+    // Post request
+    
+}
