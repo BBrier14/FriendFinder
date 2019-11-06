fb910b2a7a8966cc64f637ee96cfa59636dabcc9
Benjamin Brier <ben.brier14@gmail.com>
2019-11-06 13:08:54 -0500

    characters added, scores still needed

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+const characters = [
+    {
+        name: "Tony Soprano",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Tony_Soprano.jpg/270px-Tony_Soprano.jpg",
+        score: ["","","","","","","","","","",]
+    },
+    {
+        name: "Paulie Gualtieri",
+        photo: "https://i.pinimg.com/originals/03/c6/59/03c659249996c2eac3b1814fc7f64a58.jpg",
+        score: ["","","","","","","","","","",]
+    },
+    {
+        name: "Christopher Moltisanti",
+        photo: "https://upload.wikimedia.org/wikipedia/en/f/ff/Christopher_Moltisanti.jpg",
+        score: ["","","","","","","","","","",]
+    },
+    {
+        name: "Silvio Dante",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/the-sopranos/character/silvio-dante-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+        score: ["","","","","","","","","","",]
+    },
+    {
+        name: "Bobby Baccalieri",
+        photo: "https://upload.wikimedia.org/wikipedia/en/c/ce/BobbyBaccalieri.jpg",
+        score: ["","","","","","","","","","",]
+    },
+]
