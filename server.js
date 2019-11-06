c3b1e6662b31396e65bc9576e98a74bd1e98d515
Benjamin Brier <ben.brier14@gmail.com>
2019-11-06 12:50:12 -0500

    server.js includes dependencies, express set up, and listener

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+//========================================
+const express = require("express");
+const path = require("path");
+
+// Express Set-Up
+//========================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Express Data Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Listener
+//========================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+
+
+
