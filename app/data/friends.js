1b30aa2ecba6628d6af950d0f8da3e6ef8d8744e
Benjamin Brier <ben.brier14@gmail.com>
2019-11-06 19:26:02 -0500

    completed friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,33 @@
 const characters = [
-    {
-        name: "Tony Soprano",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Tony_Soprano.jpg/270px-Tony_Soprano.jpg",
-        score: ["","","","","","","","","","",]
-    },
-    {
-        name: "Paulie Gualtieri",
-        photo: "https://i.pinimg.com/originals/03/c6/59/03c659249996c2eac3b1814fc7f64a58.jpg",
-        score: ["","","","","","","","","","",]
-    },
-    {
-        name: "Christopher Moltisanti",
-        photo: "https://upload.wikimedia.org/wikipedia/en/f/ff/Christopher_Moltisanti.jpg",
-        score: ["","","","","","","","","","",]
-    },
-    {
-        name: "Silvio Dante",
-        photo: "https://www.hbo.com/content/dam/hbodata/series/the-sopranos/character/silvio-dante-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
-        score: ["","","","","","","","","","",]
-    },
-    {
-        name: "Bobby Baccalieri",
-        photo: "https://upload.wikimedia.org/wikipedia/en/c/ce/BobbyBaccalieri.jpg",
-        score: ["","","","","","","","","","",]
-    },
-]
+  {
+    name: "Tony Soprano",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Tony_Soprano.jpg/270px-Tony_Soprano.jpg",
+    score: ["4", "4", "4", "1", "2", "1", "3", "4", "4", "3"]
+  },
+  {
+    name: "Paulie Gualtieri",
+    photo:
+      "https://i.pinimg.com/originals/03/c6/59/03c659249996c2eac3b1814fc7f64a58.jpg",
+    score: ["5", "5", "4", "5", "1", "4", "2", "4", "2", "1"]
+  },
+  {
+    name: "Christopher Moltisanti",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/f/ff/Christopher_Moltisanti.jpg",
+    score: ["5", "3", "5", "3", "3", "3", "3", "5", "5", "5"]
+  },
+  {
+    name: "Silvio Dante",
+    photo:
+      "https://www.hbo.com/content/dam/hbodata/series/the-sopranos/character/silvio-dante-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+    score: ["2", "1", "3", "3", "4", "4", "4", "5", "1", "3"]
+  },
+  {
+    name: "Bobby Baccalieri",
+    photo: "https://upload.wikimedia.org/wikipedia/en/c/ce/BobbyBaccalieri.jpg",
+    score: ["1", "1", "3", "3", "5", "4", "3", "1", "1", "3"]
+  }
+];
 
 module.exports = characters;
