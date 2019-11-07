3a8cba98377603747a6482a6d98c840f781d88eb
Benjamin Brier <ben.brier14@gmail.com>
2019-11-06 22:36:48 -0500

    internal 500 error, not functioning

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 
+
 // Express Set-Up
 //========================================
 const app = express();
@@ -11,8 +12,6 @@ const PORT = process.env.PORT || 8080;
 // Express Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 // Router
 //========================================
